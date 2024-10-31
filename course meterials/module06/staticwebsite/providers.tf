8ce96be60fe4c655c736b851108ea1c54e111a0c
Tung No <111445323+tungno@users.noreply.github.com>
2024-10-31 22:18:23 +0100

    deploy web

--- course meterials/module06/staticwebsite/providers.tf
+++ course meterials/module06/staticwebsite/providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.73.0"
+      version = "4.7.0"
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-backend-tim"
-    storage_account_name = "sabetfstimcfw8rawacd"
-    container_name       = "tfstate"
-    key                  = "web.terraform.tfstate"
+    resource_group_name  = "rg-backend-tn"        # The name of the resource group to create the storage account in
+    storage_account_name = "sabetnnj09n"          # The name of the storage account to create
+    container_name       = "sasctn"               # The name of the blob container to create
+    key                  = "web.terraform.sasctn" # The name of the blob to store the state file in
   }
 }
 
